27810645d818044929e0d846d897d42c10f37b9e
Jauco Noordzij <j.noordzij@ogd.nl>
2014-07-08 13:46:00 +0200

    Allow spaces and weird characters in jester doc paths

--- src/lib/rebuildDocumentation.js
+++ src/lib/rebuildDocumentation.js
@@ -1,42 +1,49 @@
 var child_process = require("child_process"),
     p = require("path"),
     fs = require("fs"),
-    whenNode = require("when/node");
+    when = require("when");
 
 module.exports = function rebuildDocumentation(srcPath, targetPath, confPath, readmePath) {
-    var pathExists = function(path) {
-        return path && fs.existsSync(path);
-    };
+    return when.promise(function (resolve, reject) {
+        var pathExists = function(path) {
+            return path && fs.existsSync(path);
+        };
 
-    var ensurePathExists = function(path, msg) {
-        if(!pathExists(path)) {
-            throw new Error(msg ? msg : "path does not exist: " + path);
-        }
-    };
-
-    ensurePathExists(srcPath);
-    ensurePathExists(targetPath, "you must define a valid documentation directory by setting apiDocPath in jester.json");
-
-    // define command line options:
-    var options = "-r";
-    var src = p.resolve(srcPath);
-    var target = "-d " + p.resolve(targetPath);
-    var readme = pathExists(readmePath) ? p.resolve(readmePath) : "";
-    var conf = pathExists(confPath) ? "-c " + p.resolve(confPath) : "";
-
-    var jsdoc = require.resolve("jsdoc/jsdoc");
-    var cmd = ["node", jsdoc, readme, src, options, target, conf].join(" ");
-
-    var exec = whenNode.lift(child_process.exec);
-
-    return exec(cmd)
-        .then(
-            function(){
-                console.log("jsdoc documentation successfully written to ", targetPath);
-            },
-            function(error){
-                console.error("jsdoc failed!", error);
-                console.log("jsdoc command was: ", cmd);
+        var ensurePathExists = function(path, msg) {
+            if(!pathExists(path)) {
+                throw new Error(msg ? msg : "path does not exist: " + path);
             }
-        );
+        };
+
+        ensurePathExists(srcPath);
+        ensurePathExists(targetPath, "you must define a valid documentation directory by setting apiDocPath in jester.json");
+
+        // define command line options:
+        var readme = pathExists(readmePath) ? p.resolve(readmePath) : [];
+        var src = p.resolve(srcPath);
+        var options = ["-r"];
+        var target = ["-d", p.resolve(targetPath)];
+        var conf = pathExists(confPath) ? ["-c", p.resolve(confPath)] : [];
+
+        var jsdoc = require.resolve("jsdoc/jsdoc");
+        var args = []
+            .concat(readme)
+            .concat(src)
+            .concat(options)
+            .concat(target)
+            .concat(conf);
+
+        var process = child_process.fork(jsdoc, args);
+
+        process.on('close', function (code) {
+            if (code === 0) {
+                console.log("jsdoc documentation successfully written to ", targetPath);
+                resolve();
+            } else {
+                console.error("jsdoc failed! (exit code " + code + "). '" + jsdoc + "' was called with the following arguments:");
+                console.error(args.map(function (arg, i) { return " " + i + ": " + arg}).join("\n"));
+                reject();
+            }
+        });
+    });
 };
