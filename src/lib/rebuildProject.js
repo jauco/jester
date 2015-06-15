ae696e51a41886145399c4af314867f9f7bdf194
barbara honhoff <barbara.honhoff@ogd.nl>
2015-06-15 13:58:53 +0200

    Use webpack entry points

--- src/lib/rebuildProject.js
+++ src/lib/rebuildProject.js
@@ -1,30 +1,11 @@
 "use strict";
 
-var glob = require("../lib/globPromise"),
-    webpack = require("../lib/webpackPromise"),
-    clearDir = require("./clearDir"),
-    handleWebpackResult = require("./handleWebpackResult"),
-    p = require("path");
+var webpack = require("../lib/webpackPromise"),
+    handleWebpackResult = require("./handleWebpackResult");
 
-function createEntryModules(featureFiles) {
-    var entryModules = {};
-
-    featureFiles.forEach(function (file) {
-        var featurename = p.basename(p.dirname(file));
-        entryModules[featurename] = file;
-        console.log("    * " + featurename + " (" + file + ")." );
-    });
-
-    return entryModules;
-}
-
-module.exports = function rebuildProject(webpackConfig, entryGlob, webpackWarningFilters) {
-    return glob(entryGlob)
-        .then(function (featureFiles) {
-            var config = Object.create(webpackConfig);
-            config.entry = createEntryModules(featureFiles);
-            return webpack(config);
-        })
+module.exports = function rebuildProject(webpackConfig, webpackWarningFilters) {
+        var config = Object.create(webpackConfig);
+        return webpack(config)
         .then(function (stats){
             return handleWebpackResult(stats, webpackWarningFilters);
         });
